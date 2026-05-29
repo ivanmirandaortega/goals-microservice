e601f083677729c7ec8a30ed73340bd63dd2bacf
ivanmirandaortega <imirandaortega.pd@gmail.com>
2026-05-29 00:34:02 -0700

    updated the server port to 4001

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Test');
+	res.send('Goals Microservice Test');
 });
 
-app.listen(3000, () => {
+app.listen(4001, () => {
 	console.log(`Server running on http://localhost:3000`);
 });
